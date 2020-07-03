7a5de7fc4db2ce264decf3ab23adbf7dd34239b7
Tihon <38108567+ChastNR@users.noreply.github.com>
2020-07-03 10:33:31 +0300

    css-reset

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router } from 'react-router-dom';
 
+import '!style-loader!css-loader!@atlaskit/css-reset/dist/bundle.css';
+
 import { createBrowserHistory } from 'history';
 
 import { App } from './App';
