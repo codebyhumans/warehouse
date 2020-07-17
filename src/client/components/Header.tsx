919405def666e20ecb090d5765ea25e87b61a2f5
Tihon <38108567+ChastNR@users.noreply.github.com>
2020-07-17 22:53:00 +0300

    feat(layout): add header current active item

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -3,21 +3,22 @@ import { AtlassianLogo, AtlassianIcon } from '@atlaskit/logo';
 import { AtlassianNavigation, ProductHome, PrimaryButton } from '@atlaskit/atlassian-navigation';
 
 import { SearchDrawer } from './SearchDrawer';
-import { useHistory } from 'react-router';
+import { useHistory, useLocation } from 'react-router';
 
 const ProductHomeExample = () => <ProductHome icon={AtlassianIcon} logo={AtlassianLogo} />;
 
 export const Header = () => {
   const history = useHistory();
+  const { pathname } = useLocation();
 
   return (
     <AtlassianNavigation
       label="site"
       primaryItems={[
         <PrimaryButton>Склад</PrimaryButton>,
-        <PrimaryButton onClick={() => history.push('/users')}>Пользователи</PrimaryButton>,
-        <PrimaryButton onClick={() => history.push('/providers')}>Поставщики</PrimaryButton>,
-        <PrimaryButton onClick={() => history.push('/units')}>Ед.измерения</PrimaryButton>,
+        <PrimaryButton isHighlighted={pathname === '/users'} onClick={() => history.push('/users')}>Пользователи</PrimaryButton>,
+        <PrimaryButton isHighlighted={pathname === '/providers'} onClick={() => history.push('/providers')}>Поставщики</PrimaryButton>,
+        <PrimaryButton isHighlighted={pathname === '/units'} onClick={() => history.push('/units')}>Ед.измерения</PrimaryButton>,
       ]}
       renderSearch={SearchDrawer}
       renderProductHome={ProductHomeExample}
