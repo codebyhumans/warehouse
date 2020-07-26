11b37a3191fc96466dad1fd75cca048f6009eea3
Илья Суходольский <ilya.suhodolskiy@gmail.com>
2020-07-26 17:38:44 +0200

    refactor(deps): styled-components downgrade version

--- src/client/pages/warehouse/WarehousePage.tsx
+++ src/client/pages/warehouse/WarehousePage.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import PageHeader from '@atlaskit/page-header';
-import { Container } from '@client/theme/grid';
-import { WarehouseTree } from './WarehouseTree';
+import React from 'react'
+import PageHeader from '@atlaskit/page-header'
+import { Container } from '@client/theme/grid'
+import { WarehouseTree } from './WarehouseTree'
 
 export const WarehousePage: React.FC = () => (
   <Container>
     <PageHeader>Склад</PageHeader>
-    <WarehouseTree />
+    {/* <WarehouseTree /> */}
   </Container>
-);
+)
